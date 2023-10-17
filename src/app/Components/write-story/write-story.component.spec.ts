8ceb509d70d9871e0dc4ebf8ea3ebd2907d4fb7e
Hadeel-Hamam <hadeel.hamam.sadek@gmail.com>
2023-10-17 12:19:55 +0300

    some change

---
+++ src/app/Components/write-story/write-story.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WriteStoryComponent } from './write-story.component';
+
+describe('WriteStoryComponent', () => {
+  let component: WriteStoryComponent;
+  let fixture: ComponentFixture<WriteStoryComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [WriteStoryComponent]
+    });
+    fixture = TestBed.createComponent(WriteStoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroPageComponent } from './Components/intro-page/intro-page.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Components/login/login.component';
+import { WriteStoryComponent } from './Components/write-story/write-story.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroPageComponent,
+    HomeComponent,
+    LoginComponent,
+    WriteStoryComponent,
+    NavBarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
