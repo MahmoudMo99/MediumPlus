7b1fec54941d0c80a182853603e52b42de9e3e3a
Hadeel-Hamam <hadeel.hamam.sadek@gmail.com>
2023-11-07 10:29:52 +0200

    Hide Users From intro Page

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from 'src/app/Services/user-auth.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  isUserLogged: boolean;
+
+  constructor(private authService: UserAuthService) {
+    this.isUserLogged=this.authService.isLogged;
+  }
+  ngOnInit(): void {
+    this.authService.loggedStatus().subscribe(status=>this.isUserLogged=status)
+  }
 
 }
